5dfa337d3a4f1d5c623db1148a574fdd1abb8b9b
Bennett Hiles <bennetthiles@gmail.com>
2014-12-11 13:04:42 -0800

    Update test for package rename

--- spec/classes/tiny_alarm_spec.rb
+++ spec/classes/tiny_alarm_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'tiny_alarm' do
   it do
-    should contain_package('Tiny Alarm').with({
+    should contain_package('TinyAlarm').with({
       :provider => 'compressed_app',
       :source   => 'https://plumamazing.com/bin/tinyalarm/tinyalarm.zip',
     })
